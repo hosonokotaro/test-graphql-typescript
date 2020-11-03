a0bdecfb4b82ea43ee1720f1a2c6bab070ecc0bd
hosonokotaro <hosono1985@gmail.com>
2020-11-03 20:35:04 +0900

    copy functions

---
+++ functions/src/firebaseConfig.ts
@@ -0,0 +1,6 @@
+export default {
+  apiKey: 'AIzaSyB7k3FvoggUpNRl9NYeV4ACMedbMY3fXw4',
+  authDomain: 'test-realtime-database-2809f.firebaseapp.com',
+  databaseURL: 'https://test-realtime-database-2809f.firebaseio.com',
+  storageBucket: 'test-realtime-database-2809f.appspot.com',
+};
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,78 @@
 import * as functions from 'firebase-functions';
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { Message, MessageInput, QueryMessageArgs } from './types/graphql';
+import Crypto from 'crypto';
+import * as firebase from 'firebase/app';
+import schema from './schema';
+import firebaseConfig from './firebaseConfig';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
-});
+import 'firebase/database';
+
+// firebase settings
+
+// wip default を入れないと initializeApp 等のメソッドが存在しない
+const firebaseDefault = firebase.default;
+firebaseDefault.initializeApp(firebaseConfig);
+
+const databaseRef = (id: string) =>
+  firebaseDefault.database().ref(`posts/${id}`);
+
+// API endpoint
+// method name は Query, Mutation と同一の名前にする
+
+const root = {
+  createMessage: ({ input }: { input: MessageInput }) => {
+    const id = Crypto.randomBytes(10).toString('hex');
+
+    databaseRef(id).set({
+      content: input.content,
+      author: input.author,
+    });
+
+    return databaseRef(id)
+      .once('value')
+      .then((snapshot) => {
+        return snapshot.val();
+      });
+  },
+  message: ({ id }: QueryMessageArgs) => {
+    return databaseRef(id)
+      .once('value')
+      .then((snapshot) => {
+        return snapshot.val();
+      });
+  },
+  updateMessage: ({ input }: { input: Message }) => {
+    return databaseRef(input.id)
+      .update({
+        content: input.content,
+        author: input.author,
+      })
+      .then((_) => {
+        return input;
+      });
+  },
+  deleteMessage: ({ id }: QueryMessageArgs) => {
+    return databaseRef(id)
+      .remove()
+      .then((_) => {
+        return id;
+      });
+  },
+};
+
+// express
+
+const app = express();
+
+app.use(
+  '/',
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+export const api = functions.region('asia-northeast1').https.onRequest(app);
