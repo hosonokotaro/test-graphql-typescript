ee1bd796c3812d5bf44ccc2323e7c7e0ae047038
hosono kotaro <hosono1985@gmail.com>
2020-11-04 13:46:03 +0900

    feature/add cloudfunctions (#2)
    * add cloud functions
    
    * copy functions
    
    * move files
    
    * delete parent packages
    
    * change js to json

--- src/index.ts
+++ src/index.ts
@@ -1,80 +1,12 @@
 import express from 'express';
-import { graphqlHTTP } from 'express-graphql';
-import { Message, MessageInput, QueryMessageArgs } from './types/graphql';
-import Crypto from 'crypto';
-import * as firebase from 'firebase/app';
-import schema from './schema';
-import firebaseConfig from './firebaseConfig';
-
-import 'firebase/database';
-
-// firebase settings
-
-// wip default を入れないと initializeApp 等のメソッドが存在しない
-const firebaseDefault = firebase.default;
-firebaseDefault.initializeApp(firebaseConfig);
-
-const databaseRef = (id: string) =>
-  firebaseDefault.database().ref(`posts/${id}`);
-
-// API endpoint
-// method name は Query, Mutation と同一の名前にする
-
-const root = {
-  createMessage: ({ input }: { input: MessageInput }) => {
-    const id = Crypto.randomBytes(10).toString('hex');
-
-    databaseRef(id).set({
-      content: input.content,
-      author: input.author,
-    });
-
-    return databaseRef(id)
-      .once('value')
-      .then((snapshot) => {
-        return snapshot.val();
-      });
-  },
-  message: ({ id }: QueryMessageArgs) => {
-    return databaseRef(id)
-      .once('value')
-      .then((snapshot) => {
-        return snapshot.val();
-      });
-  },
-  updateMessage: ({ input }: { input: Message }) => {
-    return databaseRef(input.id)
-      .update({
-        content: input.content,
-        author: input.author,
-      })
-      .then((_) => {
-        return input;
-      });
-  },
-  deleteMessage: ({ id }: QueryMessageArgs) => {
-    return databaseRef(id)
-      .remove()
-      .then((_) => {
-        return id;
-      });
-  },
-};
-
-// express
 
 const app = express();
-const port = 4000;
+const port = 3000;
 
-app.use(
-  '/',
-  graphqlHTTP({
-    schema,
-    rootValue: root,
-    graphiql: true,
-  })
-);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
